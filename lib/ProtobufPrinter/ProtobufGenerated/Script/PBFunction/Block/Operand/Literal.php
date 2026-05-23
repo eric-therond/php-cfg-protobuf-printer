5ebe4882d7652838d571385c283d492dc9093360
eric-therond <eric.therond.fr@gmail.com>
2026-05-23 12:10:09 +0200

    primitive to scalar

--- lib/ProtobufPrinter/ProtobufGenerated/Script/PBFunction/Block/Operand/Literal.php
+++ lib/ProtobufPrinter/ProtobufGenerated/Script/PBFunction/Block/Operand/Literal.php
@@ -19,7 +19,7 @@ class Literal extends \Google\Protobuf\Internal\Message
      */
     protected $type = '';
     /**
-     * Generated from protobuf field <code>.protos.PrimitiveType value = 2;</code>
+     * Generated from protobuf field <code>.protos.ScalarType value = 2;</code>
      */
     protected $value = null;
 
@@ -30,7 +30,7 @@ class Literal extends \Google\Protobuf\Internal\Message
      *     Optional. Data for populating the Message object.
      *
      *     @type string $type
-     *     @type \ProtobufPrinter\ProtobufGenerated\PrimitiveType $value
+     *     @type \ProtobufPrinter\ProtobufGenerated\ScalarType $value
      * }
      */
     public function __construct($data = NULL) {
@@ -61,8 +61,8 @@ class Literal extends \Google\Protobuf\Internal\Message
     }
 
     /**
-     * Generated from protobuf field <code>.protos.PrimitiveType value = 2;</code>
-     * @return \ProtobufPrinter\ProtobufGenerated\PrimitiveType|null
+     * Generated from protobuf field <code>.protos.ScalarType value = 2;</code>
+     * @return \ProtobufPrinter\ProtobufGenerated\ScalarType|null
      */
     public function getValue()
     {
@@ -80,11 +80,11 @@ class Literal extends \Google\Protobuf\Internal\Message
     }
 
     /**
-     * Generated from protobuf field <code>.protos.PrimitiveType value = 2;</code>
-     * @param \ProtobufPrinter\ProtobufGenerated\PrimitiveType $var
+     * Generated from protobuf field <code>.protos.ScalarType value = 2;</code>
+     * @param \ProtobufPrinter\ProtobufGenerated\ScalarType $var
      * @return $this
      */
-    public function setValue(\ProtobufPrinter\ProtobufGenerated\PrimitiveType|null $var)
+    public function setValue(\ProtobufPrinter\ProtobufGenerated\ScalarType|null $var)
     {
         $this->value = $var;
 
